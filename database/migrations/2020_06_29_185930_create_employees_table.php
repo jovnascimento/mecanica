2d38b1173ab26b3c8854d41cb898866bc01d3f99
João Victor do Nascimento <jovnascimento08@hotmail.com>
2020-06-29 16:26:25 -0300

    CRUD de funcionários

---
+++ database/migrations/2020_06_29_185930_create_employees_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('rg')->unique();
+            $table->string('cpf')->unique();
+            $table->string('street');
+            $table->integer('number');
+            $table->string('complement')->nullable();
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->date('birth_date');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
